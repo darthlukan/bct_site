9c6cd882c735f4482ba0a8c5618f9660e25055d9
Brian Tomlinson <darthlukan@gmail.com>
2013-02-19 23:24:19 +0100

    Cleaned up styles.css, added contact.php script to handle the contact form added in index.html.  HTML5 form validation tips display but don't stop execution.  I need to add logic to the submit button that checks for that.

---
+++ contact.php
@@ -0,0 +1,24 @@
+<?php
+
+// Minor modifications from Contactable plugin
+// Contactable link: http://theodin.co.uk/blog/ajax/contactable-jquery-plugin.html 
+
+$name = stripcslashes($_POST['name']);
+$email = stripcslashes($_POST['email']);
+$msg = stripcslashes($_POST['msg']);
+$subject = stripcslashes($_POST['subject']);	
+$contactMessage =  
+"Message:
+		
+	$msg
+
+	Name: $name
+	E-mail: $email
+	Subject: $subject
+
+	Sending IP:$_SERVER[REMOTE_ADDR]
+	Sending Script: $_SERVER[HTTP_HOST]$_SERVER[PHP_SELF] \r\n";
+
+mail('darthlukan@gmail.com', $subject, $contactMessage);
+// Send the user back to the page
+header('Location: http://brianctomlinson.com/#contact');
