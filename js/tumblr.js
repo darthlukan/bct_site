5b96cc949c0ced9050f5cd338d09082da11f9ea3
Brian Tomlinson <darthlukan@gmail.com>
2013-03-01 19:04:06 +0100

    Added jquery tumblrfeed plugin (source: http://goo.gl/exywQ) so that I can let Tumblr deal with content management and I can focus on writing.  Edited the HTML as PyCharm found invalid stuff that was slowing the page loads down, tweaked the CSS some more to deal with the new changes, edited the tumblerfeed JS so that it would play nicer with what jQuery UI was doing with the styles, and placed my call to tumblrfeed in its own file.

---
+++ js/tumblr.js
@@ -0,0 +1,12 @@
+/**
+ * @author Brian Tomlinson
+ * @contact brian@brianctomlinson.com
+ */
+
+$('document').ready(function() {
+    $('#home-content').tumblrFeed({
+        tumblrUsername: "darthlukan",
+        apikey:"3iKw91UD5MqRAKQSKFBpgnI83eOW7l8HDNyo49WHMQDMrmSkdL",
+        postLimit:2
+    });
+});
