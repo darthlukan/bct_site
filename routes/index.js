04f33c2101cb6dcaef96054aaf775e0eb79f9232
Brian Tomlinson <brian@smartm.no>
2013-09-25 17:54:53 +0200

    spacing fixes and specify www domain rather than the other one. Oh, and route to mobile

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,7 @@
 exports.index = function (req, res) {
   res.sendfile('public/html/index.html');
 };
+
+exports.mobile = function (req, res) {
+    res.sendfile('mobile/index.html');
+};
--- server.js
+++ server.js
@@ -22,13 +22,18 @@ app.use(express.cookieParser('your secret here'));
 app.use(express.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'mobile')));
 
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+// Home
 app.get('/', routes.index);
+
+// Mobile
+app.get('/mobile', routes.mobile);
 
 // Engage
 http.createServer(app).listen(app.get('port'), app.get('ipaddr'), function(){
